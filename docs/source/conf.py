fe710f0fcdae1044144721253079e935317578db
Peter Kraus <peter.kraus@tu-berlin.de>
2023-10-25 11:34:11 +0200

    Hooking up gen-doc to sphinx.

---
+++ docs/source/conf.py
@@ -0,0 +1,90 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
+
+version = "main"
+
+# -- Project information -----------------------------------------------------
+
+project = "MaRDA Metadata Extractors"
+copyright = "2021 - 2023, yadg authors"
+author = "Peter Kraus"
+release = version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    # "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    # "sphinx.ext.autosummary",
+    # "sphinx_autodoc_typehints",
+    "sphinx_rtd_theme",
+    "myst_parser",
+    # "sphinxcontrib.autodoc_pydantic",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+# exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+html_css_files = [
+    "custom_theme.css",
+]
+html_theme_options = {
+    "body_max_width": "none",
+    "sticky_navigation": True,
+    "navigation_depth": 6,
+}
+#html_logo = "./images/yadg.png"
+#html_favicon = "./images/yadg_ico.png"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["images", "_static"]
+
+
+# -- Extension configuration -------------------------------------------------
+show_authors = True
+autosummary_generate = False
+autodoc_default_flags = ["members", "undoc-members", "show-inheritance"]
+autodoc_member_order = "bysource"
+#autodoc_pydantic_model_show_field_summary = False
+#autodoc_pydantic_model_member_order = "bysource"
+#autodoc_pydantic_model_show_field_summary = False
+#autodoc_pydantic_model_member_order = "bysource"
+intersphinx_mapping = {
+#    "dgbowl_schemas": ("https://dgbowl.github.io/dgbowl-schemas/master", None),
+#    "xarray": ("https://docs.xarray.dev/en/stable", None),
+#    "datatree": ("https://xarray-datatree.readthedocs.io/en/latest/", None),
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+MaRDA Metadata Extractors Schema
+================================
+
+A repository containing the |LinkML|_-based schemas backing the MaRDA Metadata Extractors Registry and API.
+
+The schemas implemented here are machine-actionable. They are used by the `Registry <https://github.com/marda-alliance/metadata_extractors_registry/>`_ to validate entries; a reference implementation demonstrating their pracical use is shown in the `API <https://github.com/marda-alliance/metadata_extractors_api/>`_.
+
+Contents
+````````
+The repository contains two user-facing schemas:
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Schemas
+   :hidden:
+
+   filetype/filetype
+   extractor/extractor
+
+.. |LinkML| replace:: :mod:`LinkML`
+
+.. _LinkML: https://linkml.io/linkml/
