100bb5c181a5c2ac92487d42b9cf122102f02cc5
Peter Kraus <peter.kraus@tu-berlin.de>
2023-11-01 14:49:10 +0100

    Schema repo documentation (#30)
    * Readme update.
    
    * Hooking up gen-doc to sphinx.
    
    * Add pages to CI
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * A rough structure of the docs.
    
    * Rebase & correct example path.
    
    * fix links in contributors
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Change authors name
    
    * Add deploy to CI
    
    * Mermaid should render
    
    * Fix to templates.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * wordwrap doesn't like nones.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Try out adding a logo
    
    * Matt's changes.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * github_url -> html_context.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Matthew Evans <git@ml-evs.science>

---
+++ docs/source/conf.py
@@ -0,0 +1,105 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
+
+version = "main"
+
+# -- Project information -----------------------------------------------------
+
+project = "MaRDA Metadata Extractors"
+copyright = "2022 - 2023, MaRDA Metadata Extractors Team"
+author = "Peter Kraus, Matthew Evans"
+release = version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    # "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosectionlabel",
+    # "sphinx.ext.autosummary",
+    # "sphinx_autodoc_typehints",
+    "sphinx_rtd_theme",
+    "myst_parser",
+    "sphinxcontrib.mermaid",
+    # "sphinxcontrib.autodoc_pydantic",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+html_css_files = [
+    "custom_theme.css",
+]
+
+html_theme_options = {
+    "body_max_width": "none",
+    "sticky_navigation": True,
+    "navigation_depth": 6,
+}
+html_context = {
+    "display_github": True,
+    "github_user": "marda-alliance",
+    "github_repo": "metadata_extractors_schema",
+    "github_version": version,
+    "conf_py_path": "/docs/source/"
+}
+html_logo = "https://avatars.githubusercontent.com/u/74017645?s=200&v=4"
+html_favicon = "https://www.marda-alliance.org/wp-content/uploads/2023/04/cropped-favicon-32x32.png"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["images", "_static"]
+
+
+# -- Extension configuration -------------------------------------------------
+show_authors = True
+autosummary_generate = False
+autodoc_default_flags = ["members", "undoc-members", "show-inheritance"]
+autodoc_member_order = "bysource"
+#autodoc_pydantic_model_show_field_summary = False
+#autodoc_pydantic_model_member_order = "bysource"
+#autodoc_pydantic_model_show_field_summary = False
+#autodoc_pydantic_model_member_order = "bysource"
+intersphinx_mapping = {
+#    "dgbowl_schemas": ("https://dgbowl.github.io/dgbowl-schemas/master", None),
+#    "xarray": ("https://docs.xarray.dev/en/stable", None),
+#    "datatree": ("https://xarray-datatree.readthedocs.io/en/latest/", None),
+}
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown'
+}
